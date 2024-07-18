9beb61e06f837ce5f8376514e0c2d195d5596df3
sumedhaaaaa <138441919+sumedhaaaaa@users.noreply.github.com>
2024-07-18 13:46:50 +0530

    Update ChocolateDistributionProblem.cpp

--- DAY3/ChocolateDistributionProblem.cpp
+++ DAY3/ChocolateDistributionProblem.cpp
@@ -21,3 +21,46 @@ class Solution{
     }   
 };
 
+Approach2
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+    public:
+    long long findMinDiff(vector<long long> a, long long n, long long m){
+        
+        sort(a.begin(),a.end());
+        long long mindiff=LLONG_MAX;
+        int i=0;   
+        int j=0;
+        
+        while(j<n)
+        {
+            if(j-i+1 < m)
+                j++;
+                
+            else if(j-i+1==m)
+            {
+                int diff=a[j]-a[i];
+                if(diff < mindiff)
+                    
+                        mindiff=diff;
+                    
+                       i++;
+                       j++;
+                        //j++;
+                    
+                    
+            }
+            
+        }
+          return mindiff;  
+        
+        }   
+};           
+
+
+
