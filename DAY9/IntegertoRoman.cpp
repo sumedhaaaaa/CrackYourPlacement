3e9ba9396e72402376e8ea345203edc52292ddab
sumedhaaaaa <138441919+sumedhaaaaa@users.noreply.github.com>
2024-07-24 22:09:17 +0530

    Update IntegertoRoman.cpp

--- DAY9/IntegertoRoman.cpp
+++ DAY9/IntegertoRoman.cpp
@@ -15,3 +15,34 @@ public:
         return roman;
     }
 };
+
+
+
+class Solution {
+public:
+    string intToRoman(int num) {
+        string roman;
+        map<int, string> m;
+        m[1000] = "M";
+        m[900] = "CM";
+        m[500] = "D";
+        m[400] = "CD";
+        m[100] = "C";
+        m[90] = "XC";
+        m[50] = "L";
+        m[40] = "XL";
+        m[10] = "X";
+        m[9] = "IX";
+        m[5] = "V";
+        m[4] = "IV";
+        m[1] = "I";
+        
+        for (auto it = m.rbegin(); it != m.rend(); ++it) {
+            while (num >= it->first) {
+                roman += it->second;
+                num -= it->first;
+            }
+        }
+        return roman;
+    }
+};
