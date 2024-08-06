45141862d6ad0a6ec19d2039a213fc82ef92664b
sumedhaaaaa <138441919+sumedhaaaaa@users.noreply.github.com>
2024-08-06 19:26:13 +0530

    Create IntersectionofTwoLinkedLists.cpp

---
+++ DAY22/IntersectionofTwoLinkedLists.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode* d1=headA;
+        ListNode* d2=headB;
+        
+        while(d1!=d2)
+        {
+            if(d1==NULL)
+                d1=headB;
+            else
+                d1=d1->next;
+            
+            if(d2==NULL)
+                d2=headA;
+            else
+                d2=d2->next;
+        }
+        
+        return d1;
+    }
+};
