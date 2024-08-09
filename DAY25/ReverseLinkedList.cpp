95b1c8a809e67c4848ab5a664c24dd3d5ad056b9
sumedhaaaaa <138441919+sumedhaaaaa@users.noreply.github.com>
2024-08-09 22:52:44 +0530

    Create ReverseLinkedList.cpp

---
+++ DAY25/ReverseLinkedList.cpp
@@ -0,0 +1,18 @@
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        
+    ListNode *prev = NULL;
+    ListNode *curr = head, *next;
+    while(curr!=NULL)
+    {
+        next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+    }
+    return prev;
+
+    }
+};
